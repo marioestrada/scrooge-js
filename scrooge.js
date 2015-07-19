e0ff09e6421f31012bb3e42f19577d8559b7a5c9
Mario Estrada <mestrada@yammer-inc.com>
2015-07-19 10:32:31 -0700

    Adds a new minified version

--- scrooge.js
+++ scrooge.js
@@ -1,28 +1,32 @@
-// Scrooge JS
-// Automatically adds referral information to affiliate links
-// jshint asi: true
+/* @license
+ * The MIT License (MIT)
+ *
+ *  Copyright (c) 2015 Mario Estrada (http://mario.ec)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 
-// The MIT License (MIT)
+/* Scrooge JS
+ * Automatically adds referral information to affiliate links
+ */
 
-// Copyright (c) 2015 Mario Estrada (http://mario.ec)
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+/* jshint asi: true */
 
 ;(function() {
   function domReady(fn) {
@@ -35,7 +39,7 @@
 
   Scrooge.context = document
   Scrooge.sites = {
-    'amazon': { 'url': 'amazon.com', 'param': 'tag', 'affiliate_id': '0000' },
+    'amazon': { 'url': 'amazon.com', 'param': 'tag', 'affiliate_id': 'httpmariec-20' },
     'itunes': { 'url': 'itunes.apple.com', 'param': 'affId', 'affiliate_id': '0000' }
   }
   Scrooge.cj = {
